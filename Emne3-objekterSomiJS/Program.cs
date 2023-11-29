b90e4e4cc26eb744877f5e15f7869919be20a0cc
Biner Habib <Binerhabib@gmail.com>
2023-11-29 12:30:56 +0100

    Add project files.

---
+++ Emne3-objekterSomiJS/Program.cs
@@ -0,0 +1,8 @@
+﻿using Emne3_objekterSomiJS;
+
+static void main()
+{
+    var x = new Pikachu(50, 21, "Pikachu");
+    x.PrintInfo();
+}
+main();
---
+++ Emne3-objekterSomiJS/pikachu.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Emne3_objekterSomiJS
+{
+    internal class Pikachu
+    {
+        private int Health;
+        private int Level;
+        private string Name;
+
+        public Pikachu(int health, int level, string name)
+        {
+            Health = health;
+            Level = level;
+            Name = name;
+        }
+
+        public void PrintInfo()
+        {
+            //Console.WriteLine($"Jeg er \"{Name}\". Jeg har \"{Health}\"hp og jeg er level\"{Level}");
+            Console.WriteLine($"Jeg heter {Name}. Jeg har {Health}hp og jeg er level {Level}!");
+        }
+    }
+}
